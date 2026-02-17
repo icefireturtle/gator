07a301985a29fa4d85261ac7e382a6b18c3e192d
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2026-02-16 21:39:09 -0600

    created SetUser function that updates the config file by passing the updated config struct to the Write helper function and replaces the values in the config file. Tested on main.

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,10 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
+
+const configFileName string = ".gatorconfig.json"
 
 type Config struct {
 	DBUrl string `json:"db_url"`
+	User  string `json:"user"`
 }
 
 func Read(file string) Config {
@@ -25,4 +29,40 @@ func Read(file string) Config {
 		return Config{}
 	}
 	return configFile
+}
+
+func Write(data Config) Config {
+
+	cfg := Read(configFileName)
+
+	for key, value := range map[string]string{
+		"db_url":            cfg.DBUrl,
+		"current_user_name": cfg.User,
+	} {
+		if strings.TrimSpace(value) == "" {
+			switch key {
+			case "db_url":
+				cfg.DBUrl = data.DBUrl
+			case "current_user_name":
+				cfg.User = data.User
+			}
+		}
+	}
+
+	err := os.WriteFile(configFileName, []byte(fmt.Sprintf(`{"db_url": "%s", "current_user_name": "%s"}`, cfg.DBUrl, cfg.User)), 0644)
+	if err != nil {
+		fmt.Printf("Error writing to file %s with error %s", configFileName, err)
+	}
+	return cfg
+}
+
+func SetUser(username string) {
+	if len(username) == 0 {
+		username = "username"
+	}
+
+	newUser := username
+
+	Write(Config{User: newUser})
+
 }
--- main.go
+++ main.go
@@ -10,4 +10,8 @@ func main() {
 	fmt.Println("Welcome to Gator!")
 	cfg := config.Read(".gatorconfig.json")
 	fmt.Printf("Database URL: %s\n", cfg.DBUrl)
+
+	config.SetUser("lane")
+	fmt.Printf("Set user %s in config file", cfg.User)
+
 }
